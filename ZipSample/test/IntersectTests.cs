f89afbacb0669ce2fdbb26f5178ab0840d9c3aa4
Joey Chen <jay.hatelove@gmail.com>
2018-10-26 16:24:32 +0800

    add failed test: intersect test

---
+++ ZipSample/test/IntersectTests.cs
@@ -0,0 +1,28 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZipSample.test
+{
+    [TestClass]
+    public class IntersectTests
+    {
+        [TestMethod]
+        public void intersect_integers()
+        {
+            var first = new List<int> { 1, 3, 5 };
+            var second = new List<int> { 5, 3, 7, 9 };
+
+            var expected = new List<int> { 3, 5 };
+
+            var actual = MyIntersect(first, second).ToList();
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        private IEnumerable<int> MyIntersect(IEnumerable<int> first, IEnumerable<int> second)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
