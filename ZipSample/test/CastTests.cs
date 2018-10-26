1300aaf433b62f3ca9dbbb7cf865fe6006a3cc16
Joey Chen <jay.hatelove@gmail.com>
2018-10-26 15:41:52 +0800

    move UnionTests into new File

--- ZipSample/test/CastTests.cs
+++ ZipSample/test/CastTests.cs
@@ -34,25 +34,4 @@ namespace ZipSample.test
             throw new NotImplementedException();
         }
     }
-
-    [TestClass]
-    public class UnionTests
-    {
-        [TestMethod]
-        public void Union_integers()
-        {
-            var first = new List<int> { 1, 3, 5 };
-            var second = new List<int> { 5, 3, 7, 9 };
-
-            var expected = new List<int> { 1, 3, 5, 7, 9 };
-
-            var actual = MyUnion(first, second);
-            expected.ToExpectedObject().ShouldEqual(actual);
-        }
-
-        private IEnumerable<int> MyUnion(IEnumerable<int> first, IEnumerable<int> second)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
---
+++ ZipSample/test/UnionTests.cs
@@ -0,0 +1,28 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ZipSample.test
+{
+    [TestClass]
+    public class UnionTests
+    {
+        [TestMethod]
+        public void Union_integers()
+        {
+            var first = new List<int> { 1, 3, 5 };
+            var second = new List<int> { 5, 3, 7, 9 };
+
+            var expected = new List<int> { 1, 3, 5, 7, 9 };
+
+            var actual = MyUnion(first, second);
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        private IEnumerable<int> MyUnion(IEnumerable<int> first, IEnumerable<int> second)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
