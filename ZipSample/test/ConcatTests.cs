c13ab35c4a11f6face5dd5a1d3cbeed773138f6b
Joey Chen <jay.hatelove@gmail.com>
2018-10-26 14:24:05 +0800

    add failed test: concat test

---
+++ ZipSample/test/ConcatTests.cs
@@ -0,0 +1,29 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZipSample.test
+{
+    [TestClass]
+    public class ConcatTests
+    {
+        [TestMethod]
+        public void concat_integers()
+        {
+            var first = new int[] { 1, 3, 5 };
+            var second = new int[] { 2, 4, 6 };
+
+            var actual = MyConcat(first, second).ToArray();
+
+            var expected = new int[] { 1, 3, 5, 2, 4, 6 };
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        private IEnumerable<int> MyConcat(int[] first, int[] second)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
