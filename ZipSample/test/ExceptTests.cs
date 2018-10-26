f005cf0633ad1ce52bee12f2270936e6240ff7e9
Joey Chen <jay.hatelove@gmail.com>
2018-10-26 16:37:05 +0800

    add failed test: except tests

---
+++ ZipSample/test/ExceptTests.cs
@@ -0,0 +1,28 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZipSample.test
+{
+    [TestClass]
+    public class ExceptTests
+    {
+        [TestMethod]
+        public void Except_integers()
+        {
+            var first = new List<int> { 1, 3, 5, 4 };
+            var second = new List<int> { 5, 3, 7, 9 };
+
+            var expected = new List<int> { 1, 4 };
+
+            var actual = MyExcept(first, second).ToList();
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        private IEnumerable<int> MyExcept(IEnumerable<int> first, IEnumerable<int> second)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
