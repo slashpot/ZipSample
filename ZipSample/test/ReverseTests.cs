2a1781e24d7c151f7036567ec84f16046a33839f
Joey Chen <jay.hatelove@gmail.com>
2018-10-26 11:33:16 +0800

    add failed test: MyReverse()

---
+++ ZipSample/test/ReverseTests.cs
@@ -0,0 +1,28 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZipSample.test
+{
+    [TestClass]
+    public class ReverseTests
+    {
+        [TestMethod]
+        public void reverse_string()
+        {
+            var source = new string[] { "Apple", "Banana", "Cat" };
+
+            var actual = MyReverse(source).ToList();
+            var expected = new List<string> { "Cat", "Banana", "Apple" };
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        private IEnumerable<string> MyReverse(string[] source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
